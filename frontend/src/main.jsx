557914255664037b953f2e576ad5311beb11ea7e
Luis Escobar <luispsalazar@yahoo.com>
2025-02-01 22:31:36 -0500

    frontend updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
-import { ChakraProvider } from "@/components/ui/provider"
 import React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import App from "./App.jsx"
+import { BrowserRouter } from "react-router-dom"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
