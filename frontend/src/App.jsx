c698e8577bf39c073ed58296605d5de54f7172f4
Luis Escobar <luispsalazar@yahoo.com>
2025-02-03 15:35:18 -0500

    Updated Products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
@@ -6,13 +6,14 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
       </Routes>
     </Box>
   );
 }
+
 export default App;
